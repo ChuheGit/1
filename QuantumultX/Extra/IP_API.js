6943ec7885f73126a5da755ca33b8b94912789bc
ChuheGit <QuantumultX@app.email.cn>
2020-12-14 14:00:08 +0800

    Update IP_API.js

--- QuantumultX/Extra/IP_API.js
+++ QuantumultX/Extra/IP_API.js
@@ -1,24 +1,18 @@
+// http://api.live.bilibili.com/ip_service/v1/ip_service/get_ip_addr
 if ($response.statusCode != 200) {
-  $done(Null);
+  $done(null);
 }
+
 function City_ValidCheck(para) {
   if(para) {
   return para
-  } else
-  {
-  return city0
-//emojis[getRandomInt(emojis.length)]
-  }
+  } 
 }
 
 function ISP_ValidCheck(para) {
   if(para) {
   return para
-  } else
-  {
-  return isp0
-//emojis[getRandomInt(emojis.length)]
-  }
+  } 
 }
 
 function Area_check(para) {
@@ -31,11 +25,9 @@ function Area_check(para) {
 }
 
 var body = $response.body;
-var obj = JSON.parse(body);
-var title =flags.get(obj['countryCode']) + ' '+ City_ValidCheck(obj['city']);//+Area_check(obj['country']);
-var subtitle = ISP_ValidCheck(obj['org']);
-var ip = obj['query'];
-var description = '服务商:'+obj['isp'] + '\n'+'地区:' +City_ValidCheck(obj['regionName'])+ '\n' + 'IP:'+ obj['query'] + '\n' +'时区:'+ obj['timezone'];
-
-
+var obj = JSON.parse(body)["data"];
+var title = City_ValidCheck(obj['province']);//+Area_check(obj['country']);
+var subtitle = ISP_ValidCheck(obj['isp']) + " ➠ "+ obj['country'];
+var ip = obj['addr']; 
+var description = '服务商:'+obj['isp'] + '\n'+'定位: [' +obj["latitude"]+","+obj["longitude"]+"]"+ '\n' + 'IP:'+ obj['addr'] + '\n' +'时区:'+ obj['timezone'];
 $done({title, subtitle, ip, description});
